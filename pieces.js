e20085c4362e379e3f3286ac228a9838179cd186
Quentin Guihard <quentin.guihard@outlook.fr>
2024-03-14 19:44:26 +0100

    oui

---
+++ pieces.js
@@ -0,0 +1,18 @@
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
+
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix}`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
+
+const sectionFiche = document.querySelector("fiches");
+sectionFiche.appendChild(imageElement);
+sectionFiche.appendChild(nomElement);
+sectionFiche.appendChild(prixElement);
+sectionFiche.appendChild(categorieElement);
